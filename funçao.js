817036d5f9c16acd9a1f17d2e887bc0591e63505
cristinaitako <125576531+cristinaitako@users.noreply.github.com>
2023-11-10 10:41:46 +0000

    funçao.js

--- funçao.js
+++ funçao.js
@@ -1,5 +1,10 @@
-var nome = "SeuNome";
-function saudacao(){
-    console.log("Olá,"+ nome +"!");
+function caculadoraMedia(numeros){
+    let soma = 0;
+    for (const numero of numeros){
+        soma += numero;
+    }
+    const media = soma/ numeros.length;
+    return media;
 }
-saudacao();
+const numeros = [10,20,30,40,50];
+console.log("A média é:"+ calculadoraMedia(numeros));
