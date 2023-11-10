7578bbfe27c71ee73ec923f39fbef3c20fe76048
cristinaitako <125576531+cristinaitako@users.noreply.github.com>
2023-11-10 10:48:55 +0000

    funçao.js

--- funçao.js
+++ funçao.js
@@ -1,10 +1,10 @@
-function caculadoraMedia(numeros){
+function caculadorMedia(numeros){
     let soma = 0;
     for (const numero of numeros){
         soma += numero;
     }
-    const media = soma/ numeros.length;
+    const media = soma / numeros.length;
     return media;
 }
-const numeros = [10,20,30,40,50];
-console.log("A média é:"+ calculadoraMedia(numeros));
+const numeros = [10, 20, 30, 40, 50];
+console.log("A média é: "+ calculadorMedia(numeros));
